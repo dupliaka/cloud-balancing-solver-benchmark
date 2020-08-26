990929bc26abce630c47d677b07c574767adaaf7
Anna Dupliak <adupliak@redhat.com>
2020-08-26 09:45:55 +0200

    Add black hole to avoid cashing

--- src/main/java/benchmark/BuildSolverBenchmark.java
+++ src/main/java/benchmark/BuildSolverBenchmark.java
@@ -1,25 +1,30 @@
 package benchmark;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
-import domain.CloudBalance;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 import org.optaplanner.core.api.solver.SolverFactory;
 
-@Fork(1)
-@BenchmarkMode(Mode.AverageTime)
+import domain.CloudBalance;
+
+@BenchmarkMode(Mode.SingleShotTime)
+@Fork(value = 2, warmups = 10)
 @Warmup(iterations = 10)
-@Measurement(iterations = 10)
+@Measurement(iterations = 5)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class BuildSolverBenchmark {
 
     public static void main(String[] args) throws IOException {
@@ -27,19 +32,18 @@ public class BuildSolverBenchmark {
     }
 
     @Benchmark
-    public void buildSolverFactoryCs(SolverFactoryConfigured solverFactoryConfigured) {
+    public void buildSolverFactoryCs(Blackhole blackhole, SolverFactoryConfigured solverFactoryConfigured) {
         Stream
                 .iterate(0, i -> i + 1)
                 .limit(solverFactoryConfigured.iterations)
-                .forEach(i -> solverFactoryConfigured.solverFactoryCs.buildSolver());
+                .forEach(i -> blackhole.consume(solverFactoryConfigured.solverFactoryCs.buildSolver()));
     }
 
     @Benchmark
-    public void buildSolverFactoryDrl(SolverFactoryConfigured solverFactoryConfigured) {
-        Stream.
-                iterate(0, i -> i + 1)
+    public void buildSolverFactoryDrl(Blackhole blackhole, SolverFactoryConfigured solverFactoryConfigured) {
+        Stream.iterate(0, i -> i + 1)
                 .limit(solverFactoryConfigured.iterations)
-                .forEach(i -> solverFactoryConfigured.solverFactoryDrl.buildSolver());
+                .forEach(i -> blackhole.consume(solverFactoryConfigured.solverFactoryDrl.buildSolver()));
     }
 
     @State(Scope.Benchmark)
@@ -48,7 +52,7 @@ public class BuildSolverBenchmark {
         public SolverFactory<CloudBalance> solverFactoryCs;
         public SolverFactory<CloudBalance> solverFactoryDrl;
 
-        @Param({"10", "100", "500", "1000"})
+        @Param({ "10", "100", "500", "1000" })
         public int iterations;
 
         @Setup
