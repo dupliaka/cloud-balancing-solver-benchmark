5ace1a29003afb3e19580fb7d1ea4fafea0c4d15
Anna Dupliak <adupliak@redhat.com>
2020-08-27 12:46:08 +0200

    Include results for different set of initilalization

--- src/main/java/benchmark/BuildSolverBenchmark.java
+++ src/main/java/benchmark/BuildSolverBenchmark.java
@@ -21,7 +21,7 @@ import domain.CloudBalance;
 
 @BenchmarkMode(Mode.AverageTime)
 @Fork(value = 2, warmups = 5)
-@Warmup(iterations = 5)
+@Warmup(iterations = 7)
 @Measurement(iterations = 5)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
@@ -51,7 +51,6 @@ public class BuildSolverBenchmark {
                 .forEach(i -> solverFactoryDrl.buildSolver());
         return new Object();
     }
-
     @Benchmark
     @OperationsPerInvocation(10)
     public void buildSolverFactoryCsMeasure_10(Blackhole blackhole) {
@@ -62,6 +61,42 @@ public class BuildSolverBenchmark {
     @OperationsPerInvocation(10)
     public void buildSolverFactoryDrlMeasure_10(Blackhole blackhole) {
         blackhole.consume( buildSolverFactoryDrl(10));
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(100)
+    public void buildSolverFactoryCsMeasure_100(Blackhole blackhole) {
+        blackhole.consume( buildSolverFactoryCs(100));
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(100)
+    public void buildSolverFactoryDrlMeasure_100(Blackhole blackhole) {
+        blackhole.consume( buildSolverFactoryDrl(100));
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(500)
+    public void buildSolverFactoryCsMeasure_500(Blackhole blackhole) {
+        blackhole.consume( buildSolverFactoryCs(500));
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(500)
+    public void buildSolverFactoryDrlMeasure_500(Blackhole blackhole) {
+        blackhole.consume( buildSolverFactoryDrl(500));
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(1000)
+    public void buildSolverFactoryCsMeasure_1000(Blackhole blackhole) {
+        blackhole.consume( buildSolverFactoryCs(1000));
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(1000)
+    public void buildSolverFactoryDrlMeasure_1000(Blackhole blackhole) {
+        blackhole.consume( buildSolverFactoryDrl(1000));
     }
 
 }
